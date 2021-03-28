4a406d28960a7aa20f2db7e89ddc9272a58ceb9a
TWallage <tobywallage@icloud.com>
2021-03-28 10:59:51 +0100

    Update main.py
    Adjusted some numbers
    Fixed 'divide by zero' error

--- Bouncing ball/main.py
+++ Bouncing ball/main.py
@@ -4,7 +4,7 @@ import random as rand
 from pygame.math import Vector2
 
 class Ball:
-    def __init__(self, x, y, vel_x=1, vel_y=0, accell=(0,0.15), ball_radius=20):
+    def __init__(self, x, y, vel_x=1, vel_y=0, accell=(0,0.2), ball_radius=20):
         self.pos = Vector2(x,y) #position
         self.vel = Vector2(vel_x,vel_y) #velocity
         self.acc = Vector2(accell) #accelleration
@@ -16,7 +16,7 @@ class Ball:
         speed_colour *= 13
         if speed_colour >= 359 :
             speed_colour = 359
-        self.fc.hsva = (int(speed_colour),90,90,100)
+        self.fc.hsva = (int(speed_colour),90,100,100)
         self.bc = pygame.Color(30,30,30) #dark grey
         pygame.draw.circle(window, self.bc, (self.pos.x, self.pos.y), self.ball_radius) #border
         pygame.draw.circle(window, self.fc, (self.pos.x, self.pos.y), self.ball_radius-2) #face
@@ -26,26 +26,27 @@ class Ball:
         return (distance < (self.ball_radius + ball.ball_radius)**2)
 
     def calculate_ball_collision(self, ball):
-            vel_diff = self.vel - ball.vel
+            vel_diff = self.vel-ball.vel
             pos_diff = self.pos - ball.pos
-            self.vel -= ((vel_diff).dot(pos_diff) / (pos_diff).magnitude_squared() ) * (pos_diff)# + self.acc
-            ball.vel -= ((-vel_diff).dot(-pos_diff) / (-pos_diff).magnitude_squared() ) * (-pos_diff)# + ball.acc    
-            self.vel *= 0.99
-            ball.vel *= 0.99     
+            try:
+                self.vel -= ((vel_diff).dot(pos_diff) / (pos_diff).magnitude_squared() ) * (pos_diff)# + self.acc
+                ball.vel -= ((-vel_diff).dot(-pos_diff) / (-pos_diff).magnitude_squared() ) * (-pos_diff)# + ball.acc
+            except:
+                pass
+            self.vel *= 1
+            ball.vel *= 1   
             self.pos += 0.1*pos_diff
             ball.pos -= 0.1*pos_diff
 
     def update(self):
-        self.pos += self.vel
         for ball in balls:
             if ball is self:
                 pass
             elif self.check_ball_collision(ball):
                 self.calculate_ball_collision(ball)        
-        self.check_border_collision()
-        
-
+        self.check_border_collision()        
         self.vel += self.acc
+        self.pos += self.vel       
         self.draw_ball()
             
     def check_border_collision(self):
@@ -76,13 +77,13 @@ while True:
             print('Exiting')
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            balls.append(Ball(*pygame.mouse.get_pos(), vel_x=rand.uniform(-5,5), ball_radius=rand.triangular(15,20,70)))
+            balls.append(Ball(*pygame.mouse.get_pos(), vel_x=rand.uniform(-5,5), ball_radius=rand.triangular(10,20,50)))
     window.fill(BG_COLOUR)
     for ball in balls:
         ball.update()
     pygame.display.update()
     
-    clock.tick(100)
+    clock.tick()
     
     #print(len(balls))
     #if clock.get_fps() < 90 :
